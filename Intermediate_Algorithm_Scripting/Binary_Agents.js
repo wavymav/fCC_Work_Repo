b4b50994aaee67810fdc02b8e6b4b3a1922c1dab
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-10 22:19:10 -0400

    added another intermediate algorithm solution

---
+++ Intermediate_Algorithm_Scripting/Binary_Agents.js
@@ -0,0 +1,20 @@
+// Return an English translated sentence of the passed binary string.
+
+// The binary string will be space separated.
+
+function binaryAgent(str) {
+
+  	var binArr = str.split(' ');
+  	var uniArr = [];
+
+  	binArr.forEach(function(i) {
+    	var uniCode = parseInt(i, 2);
+    	var character = String.fromCharCode(uniCode);
+    	uniArr.push(character);
+	});
+
+  	return uniArr.join('');
+
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
